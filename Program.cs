cce33127a2de3fc191bde6f5bc480c841638b1e3
Orbis Alonzo Gutierrez <38229144+Orbis25@users.noreply.github.com>
2019-02-12 04:53:15 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace PruebaWithJsonParameter
+{
+
+    class ObjectPass
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<ObjectPass> hola = new List<ObjectPass>
+            {
+                new ObjectPass()
+                {
+                   Id = 1,
+                   Name = "Hola mundo en Json desde C#"
+                }
+            };
+
+            try
+            {
+                using (StreamWriter file = File.CreateText(Path.Combine(Directory.GetCurrentDirectory(), "pass.json")))
+                {
+                    JsonSerializer serializer = new JsonSerializer();
+                    //serialize object directly into file stream
+                    serializer.Serialize(file, hola);
+                }
+                Console.WriteLine("Se escribio en el archivo .json");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Ha ocurrido un error :" + e);
+            } 
+
+            Console.ReadKey();
+        }
+    }
+}
